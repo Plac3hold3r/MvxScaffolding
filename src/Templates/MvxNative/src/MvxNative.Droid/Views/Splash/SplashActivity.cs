d306d5679c827b57bf975a51cd694f4ed1cf7409
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-11-12 19:28:22 +0200

    Add launch icon and splash icon

--- src/Templates/MvxNative/src/MvxNative.Droid/Views/Splash/SplashActivity.cs
+++ src/Templates/MvxNative/src/MvxNative.Droid/Views/Splash/SplashActivity.cs
@@ -17,7 +17,9 @@ namespace MvxNative.Droid.Views.Splash
         NoHistory = true,
         MainLauncher = true,
         Label = "@string/app_name",
-        Theme = "@style/AppTheme.Splash")]
+        Theme = "@style/AppTheme.Splash",
+        Icon = "@mipmap/ic_launcher",
+        RoundIcon = "@mipmap/ic_launcher_round")]
     public class SplashActivity : MvxSplashScreenAppCompatActivity
     {
     }
