80ff2715f8180b86731c5185f9110deb55845ac1
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-11-18 17:25:47 +0200

    Add launch screen for iOS

--- src/Templates/MvxNative/src/MvxNative.iOS/Views/!Base/BaseViewController.cs
+++ src/Templates/MvxNative/src/MvxNative.iOS/Views/!Base/BaseViewController.cs
@@ -1,10 +1,26 @@
 ﻿using MvvmCross.Core.ViewModels;
 using MvvmCross.iOS.Views;
+using MvxNative.iOS.Styles;
+using UIKit;
 
 namespace MvxNative.iOS.Views
 {
     public abstract class BaseViewController<TViewModel> : MvxViewController<TViewModel>
         where TViewModel : class, IMvxViewModel
     {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.White;
+
+            NavigationController.NavigationBar.BarStyle = UIBarStyle.Black;
+            NavigationController.NavigationBar.Translucent = false;
+            NavigationController.NavigationBar.Hidden = false;
+            NavigationController.NavigationBar.BarTintColor = ColorPalette.Primary;
+            NavigationController.NavigationBar.TintColor = UIColor.White;
+
+            NavigationController.SetNeedsStatusBarAppearanceUpdate();
+        }
     }
 }
