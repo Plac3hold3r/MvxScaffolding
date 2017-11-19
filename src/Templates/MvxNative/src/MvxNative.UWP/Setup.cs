30726d68448918a1ccd2b9a244ccb96737fe2181
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-11-19 12:44:49 +0200

    Add setup.cs and assign assets

---
+++ src/Templates/MvxNative/src/MvxNative.UWP/Setup.cs
@@ -0,0 +1,23 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Uwp.Platform;
+using MvvmCross.Uwp.Views;
+using Windows.UI.Xaml.Controls;
+
+namespace MvxNative.UWP
+{
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame, string suspensionManagerSessionStateKey = null)
+            : base(rootFrame, suspensionManagerSessionStateKey)
+        {
+        }
+
+        public Setup(IMvxWindowsFrame rootFrame)
+            : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+            => new Core.App();
+    }
+}
