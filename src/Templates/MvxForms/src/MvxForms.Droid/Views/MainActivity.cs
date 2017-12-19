e8762907d38bb5f440f2189b4e349eccf3d7f8ce
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-12-19 19:39:36 +0200

    Fix MvxForms droid back navigation

--- src/Templates/MvxForms/src/MvxForms.Droid/Views/MainActivity.cs
+++ src/Templates/MvxForms/src/MvxForms.Droid/Views/MainActivity.cs
@@ -26,6 +26,10 @@ namespace MvxForms.Droid
 
             base.OnCreate(bundle);
         }
+
+        public override void OnBackPressed()
+        {
+            MoveTaskToBack(false);
+        }
     }
 }
-
