d991aad2b8e46b902ccbae94cbdabab1f05990e5
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-12-21 16:16:09 +0200

    Update MvxForms droid

--- src/Templates/MvxForms/src/MvxForms.Droid/Views/MainActivity.cs
+++ src/Templates/MvxForms/src/MvxForms.Droid/Views/MainActivity.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
-using MvvmCross.Core.ViewModels;
-using MvvmCross.Core.Views;
-using MvvmCross.Forms.Platform;
-using MvvmCross.Forms.Droid;
 using MvvmCross.Forms.Droid.Views;
-using MvvmCross.Platform;
 using MvxForms.Core.ViewModels.Main;
 
 namespace MvxForms.Droid
