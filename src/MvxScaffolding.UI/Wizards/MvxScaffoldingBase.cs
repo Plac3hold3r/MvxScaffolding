b5152a66e719672b712e2467b8cacab60a665a7d
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-01-12 22:56:07 +0200

    Add workaround for directory and sln in VISX

--- src/MvxScaffolding.UI/Wizards/MvxScaffoldingBase.cs
+++ src/MvxScaffolding.UI/Wizards/MvxScaffoldingBase.cs
@@ -1,6 +1,8 @@
 ﻿using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio.TemplateWizard;
 using System.Collections.Generic;
+using System.IO;
 
 namespace MvxScaffolding.UI.Wizards
 {
@@ -8,22 +10,40 @@ namespace MvxScaffolding.UI.Wizards
     {
         public void BeforeOpeningFile(ProjectItem projectItem)
         {
+            // Method intentionally left empty.
         }
 
         public void ProjectFinishedGenerating(Project project)
         {
+            // Method intentionally left empty.
         }
 
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
+            // Method intentionally left empty.
         }
 
         public void RunFinished()
         {
+            // Method intentionally left empty.
         }
 
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
+            // Close new solution
+            var dte = (DTE)automationObject;
+            var solution = (Solution2)dte.Solution;
+            solution.Close();
+
+            // Delete old directory(in my case VS creating it) and change destination
+            var oldDestinationDirectory = replacementsDictionary["$destinationdirectory$"];
+            if (Directory.Exists(oldDestinationDirectory))
+            {
+                Directory.Delete(oldDestinationDirectory, true);
+            }
+
+            var newDestinationDirectory = Path.Combine($"{oldDestinationDirectory}", @"..\");
+            replacementsDictionary["$destinationdirectory$"] = Path.GetFullPath(newDestinationDirectory);
         }
 
         public bool ShouldAddProjectItem(string filePath)
---
+++ src/MvxScaffolding.UI/Wizards/MvxScaffoldingDirectoryFix.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace MvxScaffolding.UI.Wizards
+{
+    public class MvxScaffoldingDirectoryFix : IWizard
+    {
+        private DTE dte;
+        private string destinationDirectory;
+        private string solutionName;
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            // Method intentionally left empty.
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            // Method intentionally left empty.
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            // Method intentionally left empty.
+        }
+
+        public void RunFinished()
+        {
+            // Open "old" solution
+            var solution = (Solution2)dte.Solution;
+            var pathToOldSolution = Path.Combine(destinationDirectory, solutionName + ".sln");
+            solution.Open(pathToOldSolution);
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            dte = (DTE)automationObject;
+            destinationDirectory = replacementsDictionary["$destinationdirectory$"];
+            solutionName = replacementsDictionary["$safeprojectname$"];
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return false;
+        }
+    }
+}
