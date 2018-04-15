07fd5ab7826195b59984731b039599f39e95e1fc
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-04-15 19:34:59 +0200

    Add saving of last known version

--- src/MvxScaffolding.Core/Contexts/MvxScaffoldingContext.cs
+++ src/MvxScaffolding.Core/Contexts/MvxScaffoldingContext.cs
@@ -3,7 +3,9 @@
 // MvxScaffolding is licensed using the MIT License
 //---------------------------------------------------------------------------------
 
+using System;
 using System.Diagnostics;
+using MvxScaffolding.Core.Properties;
 using MvxScaffolding.Core.Template;
 
 namespace MvxScaffolding.Core.Contexts
@@ -16,6 +18,16 @@ namespace MvxScaffolding.Core.Contexts
 
         public static Stopwatch RunningTimer { get; set; }
 
-        public static string WizardVersion { get; set; } = "Local";
+        public static string WizardVersion { get; set; } = "0.0.1";
+
+        public static Version LastKnownVersion
+        {
+            get => Settings.Default.LastKnownVersion;
+            set
+            {
+                Settings.Default.LastKnownVersion = value;
+                Settings.Default.Save();
+            }
+        }
     }
 }
