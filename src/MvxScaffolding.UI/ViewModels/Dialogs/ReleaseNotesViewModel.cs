cc78b25a8d8e7c58a8d3aaf1b800f9c93124a20f
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-04-15 19:37:09 +0200

    Add release notes dialog

---
+++ src/MvxScaffolding.UI/ViewModels/Dialogs/ReleaseNotesViewModel.cs
@@ -0,0 +1,16 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright © 2018, Jonathan Froon, Plac3hold3r+github@outlook.com
+// MvxScaffolding is licensed using the MIT License
+//---------------------------------------------------------------------------------
+
+using System.Windows.Input;
+using MvxScaffolding.Core.Configuration;
+using MvxScaffolding.Core.Template;
+using MvxScaffolding.UI.Commands;
+
+namespace MvxScaffolding.UI.ViewModels.Dialogs
+{
+    public class ReleaseNotesViewModel : AboutViewModel
+    {
+    }
+}
