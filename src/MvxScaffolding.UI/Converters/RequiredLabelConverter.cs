c5f8443ebd3d900f9ae5c380484240f3dd245fa8
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-04-21 18:52:18 +0200

    Add required label converter

---
+++ src/MvxScaffolding.UI/Converters/RequiredLabelConverter.cs
@@ -0,0 +1,24 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright © 2018, Jonathan Froon, Plac3hold3r+github@outlook.com
+// MvxScaffolding is licensed using the MIT License
+//---------------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MvxScaffolding.UI.Converters
+{
+    public class RequiredLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{value} *";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
