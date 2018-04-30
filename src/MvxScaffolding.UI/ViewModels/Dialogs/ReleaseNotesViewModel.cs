c28bf8e36ef1e5e17a87cec2c71863b756368767
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-04-30 22:58:59 +0200

    Update language usage for release notes

--- src/MvxScaffolding.UI/ViewModels/Dialogs/ReleaseNotesViewModel.cs
+++ src/MvxScaffolding.UI/ViewModels/Dialogs/ReleaseNotesViewModel.cs
@@ -4,17 +4,40 @@
 //---------------------------------------------------------------------------------
 
 using System.IO;
+using System.Windows.Input;
+using MvxScaffolding.Core.Configuration;
+using MvxScaffolding.Core.Template;
+using MvxScaffolding.UI.Commands;
 
 namespace MvxScaffolding.UI.ViewModels.Dialogs
 {
-    public class ReleaseNotesViewModel : AboutViewModel
+    public class ReleaseNotesViewModel : BaseViewModel
     {
+        public ICommand GoToGitHubCommand { get; }
+        public ICommand GoToChangelogCommand { get; }
+
         private string _releaseNotes;
 
         public string ReleaseNotes
         {
             get => _releaseNotes;
             set { _releaseNotes = value; OnPropertyChanged(nameof(ReleaseNotes)); }
+        }
+
+        public ReleaseNotesViewModel()
+        {
+            GoToGitHubCommand = new RelayCommand(GoToGitHubLink);
+            GoToChangelogCommand = new RelayCommand(GoToChangelogLink);
+        }
+
+        private void GoToGitHubLink()
+        {
+            OpenLink(Config.Current.GitHubUri, TemplateLinks.GitHub);
+        }
+
+        private void GoToChangelogLink()
+        {
+            OpenLink(Config.Current.ChangelogUri, TemplateLinks.Changelog);
         }
 
         public override void OnDialogOpened()
