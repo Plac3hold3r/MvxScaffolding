1b740a35038842f88a97c09d2c3ec4ab6ec760d1
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-08-15 21:28:48 +0200

    Fix for native menu toolbar assignment

--- src/Templates/MvxNative/NavigationMenu/src/MvxNative.Droid/Views/!Base/BaseActivity.cs
+++ src/Templates/MvxNative/NavigationMenu/src/MvxNative.Droid/Views/!Base/BaseActivity.cs
@@ -19,13 +19,15 @@ namespace MvxNative.Droid.Views
     {
         protected abstract int ActivityLayoutId { get; }
 
-        protected Toolbar Toolbar => FindViewById<Toolbar>(Resource.Id.toolbar);
+        protected Toolbar Toolbar { get; private set; }
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(ActivityLayoutId);
+
+            Toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
 
             SetSupportActionBar(Toolbar);
         }
