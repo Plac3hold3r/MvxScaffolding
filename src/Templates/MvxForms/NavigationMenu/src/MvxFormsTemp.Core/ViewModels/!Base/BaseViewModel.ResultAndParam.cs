590acd6556b7a1f755835b34245efb8616bd9919
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-09-14 22:28:26 +0200

    Update view model namespace order

--- src/Templates/MvxForms/NavigationMenu/src/MvxFormsTemp.Core/ViewModels/!Base/BaseViewModel.ResultAndParam.cs
+++ src/Templates/MvxForms/NavigationMenu/src/MvxFormsTemp.Core/ViewModels/!Base/BaseViewModel.ResultAndParam.cs
@@ -1,7 +1,7 @@
-﻿using MvvmCross.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MvvmCross.ViewModels;
 
 namespace MvxFormsTemp.Core.ViewModels
 {
--- src/Templates/MvxForms/SingleView/src/MvxFormsTemp.Core/ViewModels/!Base/BaseViewModelResult.cs
+++ src/Templates/MvxForms/SingleView/src/MvxFormsTemp.Core/ViewModels/!Base/BaseViewModelResult.cs
@@ -1,8 +1,8 @@
-﻿using MvvmCross.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using MvvmCross.ViewModels;
 
 namespace MvxFormsTemp.Core.ViewModels
 {
