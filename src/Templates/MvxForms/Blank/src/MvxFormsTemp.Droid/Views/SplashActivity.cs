e2c2283734a8c2ea15070cc016f2bce27d549fc0
Plac3hold3r <Plac3hold3r+github@outlook.com>
2020-08-14 20:31:33 +0200

    feat: update Nuget packages for MvxForms

--- src/Templates/MvxForms/Blank/src/MvxFormsTemp.Droid/Views/SplashActivity.cs
+++ src/Templates/MvxForms/Blank/src/MvxFormsTemp.Droid/Views/SplashActivity.cs
@@ -10,7 +10,7 @@ namespace MvxFormsTemp.Droid.Views
        MainLauncher = true,
        Label = "@string/app_name",
        Theme = "@style/AppTheme.Splash")]
-    public class SplashActivity : MvxFormsSplashScreenAppCompatActivity<Setup, Core.App, UI.App>
+    public class SplashActivity : MvxFormsSplashScreenActivity<Setup, Core.App, UI.App>
     {
         protected override Task RunAppStartAsync(Bundle bundle)
         {
