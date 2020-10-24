770badf4327dc567829aaafe04cd854df10c9211
Plac3hold3r <Plac3hold3r+github@outlook.com>
2020-10-24 17:18:55 +0200

    feat: update to notnull over the class constraint

--- src/Templates/MvxForms/NavigationMenu/src/MvxFormsTemp.Core/ViewModels/BaseViewModel.ResultAndParam.cs
+++ src/Templates/MvxForms/NavigationMenu/src/MvxFormsTemp.Core/ViewModels/BaseViewModel.ResultAndParam.cs
@@ -6,8 +6,8 @@ using MvvmCross.ViewModels;
 namespace MvxFormsTemp.Core.ViewModels
 {
     public abstract class BaseViewModel<TParameter, TResult> : BaseViewModelResult<TResult>, IMvxViewModel<TParameter, TResult>
-        where TParameter : class
-        where TResult : class
+        where TParameter : notnull
+        where TResult : notnull
     {
         public abstract void Prepare(TParameter parameter);
     }
--- src/Templates/MvxNative/SingleView/src/MvxNative.Core/ViewModels/BaseViewModel.Param.cs
+++ src/Templates/MvxNative/SingleView/src/MvxNative.Core/ViewModels/BaseViewModel.Param.cs
@@ -6,7 +6,7 @@ using MvvmCross.ViewModels;
 namespace MvxNative.Core.ViewModels
 {
     public abstract class BaseViewModel<TParameter> : BaseViewModel, IMvxViewModel<TParameter>
-        where TParameter : class
+        where TParameter : notnull
     {
         public abstract void Prepare(TParameter parameter);
     }
