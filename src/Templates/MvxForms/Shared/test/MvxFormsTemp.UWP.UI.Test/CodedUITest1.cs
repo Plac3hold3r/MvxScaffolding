d7bb29ae77f9532da6a510e4292bf32447b55704
Plac3hold3r <Plac3hold3r+github@outlook.com>
2021-10-16 17:20:23 +0200

    feat: switch UWP UI test with Appium with WinAppDriver

--- src/Templates/MvxForms/Shared/test/MvxFormsTemp.UWP.UI.Test/CodedUITest1.cs
+++ src/Templates/MvxForms/Shared/test/MvxFormsTemp.UWP.UI.Test/CodedUITest1.cs
@@ -1,82 +1,81 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UITesting;
+using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UITest.Input;
-using Microsoft.VisualStudio.TestTools.UITest.Extension;
-using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
-using Microsoft.VisualStudio.TestTools.UITesting.DirectUIControls;
-using Microsoft.VisualStudio.TestTools.UITesting.WindowsRuntimeControls;
-using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
 
 
 namespace MvxFormsTemp.UWP.UI.Test
 {
-    /// <summary>
-    /// Summary description for CodedUITest1
-    /// </summary>
-    [CodedUITest(CodedUITestType.WindowsStore)]
+    [TestClass]
     public class CodedUITest1
     {
-        public CodedUITest1()
+        // TODO WTS: install WinAppDriver and start it before running tests: https://github.com/Microsoft/WinAppDriver
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+
+        // TODO WTS: set the app launch ID.
+        // The part before "!App" will be in Package.Appxmanifest > Packaging > Package Family Name.
+        // The app must also be installed (or launched for debugging) for WinAppDriver to be able to launch it.
+        protected const string AppToLaunch = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX_XXXXXXXXXXXXX!App";
+
+        protected static WindowsDriver<WindowsElement> AppSession { get; set; }
+
+        private static string _screenshotFolder;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
         {
+            // TODO WTS: change the location where screenshots are saved.
+            // Create separate folders for saving the results of each test run.
+            _screenshotFolder = $"{Path.GetPathRoot(Environment.CurrentDirectory)}\\Temp\\Screenshots\\{DateTime.Now.ToString("dd_HHmm")}\\";
+
+            // Make sure the folder exists or saving screenshots will fail.
+            if (!Directory.Exists(_screenshotFolder))
+            {
+                Directory.CreateDirectory(_screenshotFolder);
+            }
         }
 
+        [TestInitialize]
+        public void LaunchApp()
+        {
+            if (AppSession == null)
+            {
+                var appiumOptions = new AppiumOptions();
+                appiumOptions.AddAdditionalCapability("app", AppToLaunch);
+                appiumOptions.AddAdditionalCapability("deviceName", "WindowsPC");
+                AppSession = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appiumOptions);
+
+                Assert.IsNotNull(AppSession, "Unable to launch app.");
+
+                AppSession.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+
+                // Maximize the window to have a consistent size and position.
+                AppSession.Manage().Window.Maximize();
+            }
+        }
+
+        // TODO WTS: Add other tests as appropriate.
         [TestMethod]
-        public void CodedUITestMethod1()
+        public void TakeScreenshotOfLaunchPage()
         {
-            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            var screenshotFileName = Path.Combine(_screenshotFolder, $"{Path.GetRandomFileName()}.png");
+
+            var screenshot = AppSession.GetScreenshot();
+            screenshot.SaveAsFile(screenshotFileName, ScreenshotImageFormat.Png);
+
+            Assert.IsTrue(File.Exists(screenshotFileName));
         }
 
-        #region Additional test attributes
-
-        // You can use the following additional attributes as you write your tests:
-
-        ////Use TestInitialize to run code before running each test 
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{        
-        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-        //}
-
-        ////Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{        
-        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-        //}
-
-        #endregion
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
+        [TestCleanup]
+        public void TearDown()
         {
-            get
+            if (AppSession != null)
             {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
+                AppSession.Dispose();
+                AppSession = null;
             }
         }
-        private TestContext testContextInstance;
-
-        public UIMap UIMap
-        {
-            get
-            {
-                if (this.map == null)
-                {
-                    this.map = new UIMap();
-                }
-
-                return this.map;
-            }
-        }
-
-        private UIMap map;
     }
 }
